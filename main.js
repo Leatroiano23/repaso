6b14ccb02aafa2ce9a92c2d243f38c64bd4cd5be
unknown <maralelu2009@hotmail.com>
2024-08-19 19:28:28 -0300

    repaso

---
+++ main.js
@@ -0,0 +1,93 @@
+///.1
+//function numParImpar(num) {
+//if (num % 2 === 0) {
+// console.log("el numero es par");
+//} else {
+//  console.log("el numero es impar");
+// }
+//}
+//numParImpar(22);
+
+// //.2
+// const numMayorIgual = (num1, num2) => {
+//   if (num1 > num2) {
+//     console.log("num1 es mayor");
+//   } else if (num1 < num2) {
+//     console.log("num2 es mayor");
+//   } else {
+//     console.log("son iguales");
+//   }
+// };
+
+// console.log(numMayorIgual(17, 18));
+
+//.3;
+
+// const multiplo5 = (num) => {
+//   if (num % 5 === 0) console.log("num es multiplo de cinco");
+//   else {
+//     console.log("no es multiplo de 5");
+//   }
+// };
+
+// console.log(multiplo5(19));
+
+//.4
+
+// function suma(diez) {
+//   for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+//   }
+// }
+// suma();
+
+// .5
+// function ejercicio5(boca, num) {
+//   for (let i = 0; i < 10; i++) {
+//     console.log(boca);
+//   }
+// }
+// ejercicio5("boca", 23);
+
+//.6
+
+// const bocaJuniors = [
+//   "advincula",
+//   "cavani",
+//   "merentiel",
+//   "zenon",
+//   "medina",
+//   "blanco",
+//   "romero",
+// ];
+// function ejerci6(boca) {
+//   for (let i = 0; i < bocaJuniors.length; i++) {
+//     console.log(boca[i]);
+//   }
+// }
+// ejerci6(bocaJuniors);
+
+//7
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function ejerci7(salteo) {
+//   for (let i = 0; i < salteo.length; i++) {
+//     if (i === 4) {
+//       continue;
+//     }
+//     console.log(salteo[i]);
+//   }
+// }
+// ejerci7(num);
+
+//8
+
+// const num = [1, 2, 3, 4, 5, 6];
+// const multiplicador = 5;
+
+// function ejerci8(dieciseies, uno) {
+//   for (let i = 0; i < dieciseies.length; i++) {
+//     console.log(dieciseies[i] * uno);
+//   }
+// }
+
+// ejerci8(num, multiplicador);
